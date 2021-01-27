8f30848352479facb6f42abae3e44bacfa89898c
Watakumi <watauta22@gmail.com>
2021-01-28 02:56:42 +0900

    bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_175131) do
+ActiveRecord::Schema.define(version: 2021_01_27_175458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "image"
+    t.bigint "story_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["story_id"], name: "index_cards_on_story_id"
+  end
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_01_27_175131) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cards", "stories"
 end
