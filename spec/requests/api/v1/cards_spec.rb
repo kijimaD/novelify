7205695f8f79e0e4b41ad4f79b45ea5e1e7f272a
kijima <norimaking777@gmail.com>
2021-01-29 22:21:47 +0900

    [Fix]URL変更で影響を受けた部分を修正した

--- spec/requests/api/v1/cards_spec.rb
+++ spec/requests/api/v1/cards_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Card', type: :request do
-  describe 'GET /api/v1/cards' do
+
+  let(:story) { create(:story) }
+  let(:card) { create(:card, image: 'test-image') }
+
+  describe 'GET /api/v1/stories/:id/cards' do
     it '全てのcardを取得する' do
       create_list(:card, 10)
 
-      get '/api/v1/cards'
+      get "/api/v1/stories/#{story.id}/cards"
       json = JSON.parse(response.body)
 
       expect(response.status).to eq(200)
@@ -13,11 +17,9 @@ RSpec.describe 'Card', type: :request do
     end
   end
 
-  describe 'GET /api/v1/cards/:id' do
+  describe 'GET /api/v1/stories/:id/cards/:id' do
     it '特定のcardを取得する' do
-      card = create(:card, image: 'test-image')
-
-      get "/api/v1/cards/#{card.id}"
+      get "/api/v1/stories/#{story.id}/cards/#{card.id}"
       json = JSON.parse(response.body)
 
       expect(response.status).to eq(200)
@@ -25,21 +27,17 @@ RSpec.describe 'Card', type: :request do
     end
   end
 
-  describe 'POST /api/v1/cards' do
+  describe 'POST /api/v1/stories/:id/cards' do
     it '新しいcardを作成する' do
-      story = create(:story)
-      valid_params = { image: 'image', story_id: story.id }
-
-      expect { post '/api/v1/cards', params: { card: valid_params } }.to change(Card, :count).by(1)
+      valid_params = { image: 'image' }
+      expect { post "/api/v1/stories/#{story.id}/cards", params: { card: valid_params } }.to change(Card, :count).by(1)
       expect(response.status).to eq(200)
     end
   end
 
-  describe 'PUT /api/v1/cards/:id' do
+  describe 'PUT /api/v1/stories/:id/cards/:id' do
     it 'cardの編集を行う' do
-      card = create(:card, image: 'old-image')
-
-      put "/api/v1/cards/#{card.id}", params: { card: {image: 'new-image'}  }
+      put "/api/v1/stories/#{story.id}/cards/#{card.id}", params: { card: {image: 'new-image'}  }
       json = JSON.parse(response.body)
 
       expect(response.status).to eq(200)
@@ -47,11 +45,11 @@ RSpec.describe 'Card', type: :request do
    end
   end
 
-  describe 'DELETE /api/v1/card/:id' do
+  describe 'DELETE /api/v1/stories/:id/card/:id' do
     it 'cardを削除する' do
       card = create(:card)
 
-      expect { delete "/api/v1/cards/#{card.id}" }.to change(Card, :count).by(-1)
+      expect { delete "/api/v1/stories/#{story.id}/cards/#{card.id}" }.to change(Card, :count).by(-1)
       expect(response.status).to eq(200)
     end
   end
