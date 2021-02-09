1be44a9505c4d0033b57e3874992ee71a5f8075a
石渡拓海 <t-ishiwata@sight-visit.com>
2021-02-09 19:26:56 +0900

    bundle exec rails g graphql:object Story

---
+++ app/graphql/types/story_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class StoryType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
